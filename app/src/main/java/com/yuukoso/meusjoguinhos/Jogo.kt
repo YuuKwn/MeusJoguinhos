3d37512c3fa847343acf6cba1e2c9683c4b0c47b
logonrm <logonrm@fiap.com.br>
2018-05-04 10:12:55 -0300

    Initial commit

---
+++ app/src/main/java/com/yuukoso/meusjoguinhos/Jogo.kt
@@ -0,0 +1,6 @@
+package com.yuukoso.meusjoguinhos
+
+data class Jogo(val imagemId: Int,
+                val titulo: String,
+                val anoLancamento: Int,
+                val descricao: String)
---
+++ app/src/main/java/com/yuukoso/meusjoguinhos/MyGamerus.kt
@@ -0,0 +1,12 @@
+package com.yuukoso.meusjoguinhos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MyGamerus : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_gamerus)
+    }
+}
